ff663da00d978711a0b195da557be2cbc5aca596
Melay <sashamelay@gmail.com>
2021-09-02 22:24:30 +0300

    finish

---
+++ app.js
@@ -0,0 +1,16 @@
+let name = prompt('What is your name');
+console.log(name);
+let lastName = prompt('What is yout lastname?');
+console.log(lastName);
+let fullName = name + ' ' + lastName;
+
+const age = confirm('Are u 18?');
+
+if(age) {
+    console.log('yes');
+    alert(fullName + ' access allowed')
+}
+else {
+    console.log('no');
+    alert(fullName + ' access denied')
+}
